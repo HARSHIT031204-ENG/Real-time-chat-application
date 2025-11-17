71062dac81051c9d1c32561833197f17f9448ffa
Harshit Garg <gargh2120@gmail.com>
2025-11-17 23:47:25 +0530

    update dependency & fixing error

--- Server/app.js
+++ Server/app.js
@@ -6,38 +6,31 @@ import cors from "cors";
 const port = 3000;
 const app = express();
 
-
 app.use(cors({
-    origin : "http://localhost:5173",
-    methods: ["POST", "GET", "PUT"],
-    credentials : true
+  origin: "http://localhost:5173",
+  methods: ["POST", "GET"],
+  credentials: true
 }));
-
 
 const server = createServer(app);
 
-// const io = new Server(server);
-
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["POST", "GET", "PUT"],
-        credentials: true
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
 });
-
 
 io.on("connection", (socket) => {
-    console.log("user connected! success ");
-    console.log("user connected socket id ", socket.id);
-    socket.emit("welcome", "welcome to the server ")
+  console.log("user connected", socket.id);
+
+  socket.emit("welcome", "Welcome to the server");
+
+  // listen for message from client
+  socket.on("send-message", (msg) => {
+    console.log("message from client:", msg);
+    socket.broadcast.emit("message", msg); // send to all clients
+  });
 });
 
-app.get("/", (req, res) => {
-    res.send("hello brother odwkjbkjebk");
-    console.log("hello world !");
-});
-
-server.listen(port, () => {
-    console.log(`server is listening at port ${port}`);
-});
+server.listen(port, () => console.log(`Server running on ${port}`));
