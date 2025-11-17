1bc9abd161233283f118b03cae98f7eef9c3e855
Harshit Garg <gargh2120@gmail.com>
2025-11-17 23:41:18 +0530

    update and fixing error

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,27 @@
-import { useEffect } from "react"
-import {io} from "socket.io-client"
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import Hero from "./HeroSection";
+
+const socket = io("http://localhost:3000");   // created only once
 
 function App() {
-  const socket = io("http://localhost:3000")
+  const [socketID, setsocketID] = useState("");
 
   useEffect(() => {
     socket.on("connect", () => {
-      console.log("connect", socket.id);
-    })
+      console.log("connected new socket ", socket.id);
+    });
 
-    socket.on("welcome", (s) => {console.log(s);
-    })
-  })
-  return (
-    <>harshit </>
-  )
+    socket.on("welcome", (msg) => {
+      console.log("server says:", msg);
+    });
+
+    socket.on("message", (msg) => {
+      console.log("message from server:", msg);
+    });
+  }, []);
+
+  return <Hero socket={socket} />;
 }
 
-export default App
+export default App;
